4d46807d3fe1ed5036195371bab474196e5184c7
George <45779577+George-Catalin@users.noreply.github.com>
2020-03-14 01:55:33 +0000

    Add files via upload

---
+++ Client-Server-ChatApp/readme.txt
@@ -0,0 +1,5 @@
+Group members:
+
+George-Catalin Muselin 001027955
+Simeon Tudzharov 001040808
+Razaq Taiwo 001035380
---
+++ Client-Server-ChatApp/src/menuItem.java
@@ -0,0 +1,32 @@
+
+import javax.swing.KeyStroke;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Simeon
+ */
+class menuItem {
+
+    static void setMnemonic(int VK_C) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static void setAccelerator(KeyStroke keyStroke) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static void setActionCommand(String quit) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static void addActionListener(ClientGUI aThis) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
